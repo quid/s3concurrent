a1eac58fca26b41fbb3b37011852c86e9b299a60
Robert Roose <rroose@quid.com>
2016-04-07 14:37:56 -0700

    Moved pip dependencies into requirements.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from distutils.core import setup
-
-install_requires=[
-    "boto>=2.32.1",
-    "argparse>=1.3.0",
-    "mock==1.0.1",
-    "nose==1.3.4"
-]
+from pip.req import parse_requirements
 
 setup(
     name='s3concurrent',
-    version='0.2.1',
+    version='0.2.2',
     author='Quid Inc.',
     author_email='ops@quid.com',
     packages=['s3concurrent'],
@@ -19,7 +13,9 @@ setup(
     description='A fast S3 downloader/uploader for deep file structures.',
     keywords='s3 download upload tools',
     long_description=open('README.md').read(),
-    install_requires=install_requires,
+    install_requires=(str(ir.req) for ir in \
+        parse_requirements('requirements.txt', session=False)
+    ),
     entry_points={
     'console_scripts': [
         's3concurrent_download=s3concurrent.s3concurrent:s3concurrent_download',
